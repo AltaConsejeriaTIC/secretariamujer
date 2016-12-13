ef8461c93b074ae6527a38cca6065b06d71fe5ac
Andres Diaz <andiazvar@gmail.com>
2016-12-13 16:31:20 -0500

    added heat map demo app

---
+++ app/src/pages/home/home.spec.ts
@@ -0,0 +1,10 @@
+import { HomePage }                      from './home';
+let instance: HomePage = null;
+
+describe('dummy set test', () => {
+
+  it('dummy test', () => {
+    expect(2).toEqual(2);
+  });
+
+});
