ccb262f85ef728ea83629b8676751856a20e0ce0
AndresDiaz1 <andiazvar@gmail.com>
2016-12-19 11:19:44 -0500

    Drupal installed with apache2

--- administrator/config-dev-environment.sh
+++ administrator/config-dev-environment.sh
@@ -3,5 +3,8 @@ npm install
 sudo apt install php -y
 sudo apt install php7.0-gd php7.0-mysql php7.0-dom -y
 sudo apt install mysql-server -y
+echo "drop database appsofia" | mysql -u root -ptoor
 echo "create database appsofia" | mysql -u root -ptoor
 mysql -u root -ptoor < db/db.sql
+
+sudo cp 000-default.conf /etc/apache2/sites-enabled
