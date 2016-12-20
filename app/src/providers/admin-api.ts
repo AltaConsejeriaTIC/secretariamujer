66af34a1bc75ce09a4d28c1dd8d03d27b92afa9b
AndresDiaz1 <andiazvar@gmail.com>
2016-12-20 18:11:34 -0500

    Can log in as app user, and then get admin user data

--- app/src/providers/admin-api.ts
+++ app/src/providers/admin-api.ts
@@ -1,45 +1,49 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
-  Generated class for the AdminAPI provider.
+ Generated class for the AdminAPI provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
 @Injectable()
 export class AdminAPI {
 
   constructor(public http: Http) {
   }
 
-   outputPrint(){
-    console.log("esta imprimiendo", 9+9);
+  outputPrint() {
+    console.log("esta imprimiendo", 9 + 9);
   }
 
-  loginAsAppUser(){
-     let name = "app";
-     let pass="app";
-     let body=JSON.stringify({ name, pass })
-     this.http.post('http://192.168.88.246/admin/user/login?_format=json',body)
-       .subscribe(res=>{
-         console.log("lo que respondio", res)
-       },
-       err=>{
-         console.log("hubo un error", err)
-       })
+  loginAsAppUser() {
+    let name = "app";
+    let pass = "app";
+    let body = JSON.stringify({name, pass})
 
+    this.http.post('http://192.168.88.246/admin/user/login?_format=json', body).map(res => res.json())
+      .subscribe(res => {
+          console.log("lo que respondio", res);
+          this.getusers();
+        },
+        err => {
+          console.log("hubo un error 2", err)
+        })
   }
 
-  getusers(){
-     this.http.get('http://192.168.88.246/admin/user/1?_format=json').map(res=>res.json())
-       .subscribe(data=>{
+  getusers() {
+    let headers = new Headers({'Content-Type': 'application/json', 'Authorization':'Basic '+'YXBwOmFwcA=='}); //Base64Encode name:pass
+    let options = new RequestOptions({headers: headers});
+
+    this.http.get('http://192.168.88.246/admin/user/1?_format=json', options).map(res => res.json())
+      .subscribe(data => {
           console.log("lo que recibio en get users", data)
-     },
-      err=>{
-        console.log("hubo un error en get users", err)
-      })
+        },
+        err => {
+          console.log("hubo un error en get users", err)
+        })
   }
 
 }
