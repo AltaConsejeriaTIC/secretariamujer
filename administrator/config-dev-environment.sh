46611f4aa5f802a36ada2b2e3868922ecf5d8dbe
Víctor Rodriguez <vrodmz@gmail.com>
2016-12-22 15:26:00 -0500

    Script for setup the develop enviroment updated to the actual process

--- administrator/config-dev-environment.sh
+++ administrator/config-dev-environment.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 npm install
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
 sudo apt install php -y
 sudo apt install php7.0-gd php7.0-mysql php7.0-dom -y
 sudo apt install mysql-server -y
+
 echo "drop database appsofia" | mysql -u root -ptoor
 echo "create database appsofia" | mysql -u root -ptoor
 mysql -u root -ptoor < db/db.sql
 
-sudo cp 000-default.conf /etc/apache2/sites-enabled
+sudo cp appsofia.com.conf /etc/apache2/sites-enabled
+sudo cp ports.conf /etc/apache2
+sudo a2enmod rewrite
+sudo a2enmod headers
+chmod 664 drupal/sites/default/settings.php
+sudo chown -R :www-data drupal/*
+sudo service apache2 restart
