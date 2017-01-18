1f802ffe76c0fecebe92ab74d0498ac10c99bc3c
Víctor Rodriguez <vrodmz@gmail.com>
2017-01-18 12:49:11 -0500

    Error that didn't allow create a build fixed

--- app/src/pages/select-category/select-category.ts
+++ app/src/pages/select-category/select-category.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {SelectCategoryService} from "../../providers/select-category-service";
-import {category} from "../../entity/category";
 import {TestPage} from "../test-page/test-page";
 import {MenuPage} from "../menu/menu";
 
@@ -10,32 +9,12 @@ import {MenuPage} from "../menu/menu";
   templateUrl: './select-category.html'
 })
 export class SelectCategoryPage {
-
-  categories: category[] = [
-    {
-      category: 'Violencia Económica',
-      RESTAddress: 'preguntas-violencia-economica'
-    },
-    {
-      category: 'Violencia Físca',
-      RESTAddress: 'preguntas-violencia-fisica'
-    },
-    {
-      category: 'Violencia Psicológica',
-      RESTAddress: 'preguntas-violencia-psicologica'
-    },
-    {
-      category: 'Violencia Sexual',
-      RESTAddress: 'preguntas-violencia-sexual'
-    }
-  ];
-
   constructor(public navController: NavController, public selectCategoryService: SelectCategoryService) {
-    this.categories = this.selectCategoryService.fillCategoriesWithEmptyObjects();
+
   }
 
   ionViewDidLoad() {
-    this.categories = this.selectCategoryService.getCategories();
+
   }
 
   selectCategory(categoryId: number) {
--- app/src/pages/welcome-test/welcome-test.ts
+++ app/src/pages/welcome-test/welcome-test.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {SelectCategoryService} from "../../providers/select-category-service";
 import {SelectCategoryPage} from "../select-category/select-category";
+import {MenuPage} from "../menu/menu";
 
 @Component({
   selector: 'page-welcome-test',
@@ -9,23 +10,28 @@ import {SelectCategoryPage} from "../select-category/select-category";
 })
 export class WelcomeTestPage {
 
-  constructor(public navCtrl: NavController, public selectCategoyService:SelectCategoryService) {}
+  constructor(public navController: NavController, public selectCategoyService: SelectCategoryService) {
+  }
 
   ionViewDidLoad() {
     console.log('Hello WelcomeTestPage Page');
   }
 
-  goToTestSelectTestCategory(){
+  goToTestSelectTestCategory() {
     this.selectCategoyService.setCategory('tests');
-    this.navCtrl.push(SelectCategoryPage);
+    this.navController.push(SelectCategoryPage);
   }
 
-  goToTips(){
+  goToTips() {
 
   }
 
-  goToRoutes(){
+  goToRoutes() {
 
+  }
+
+  goToMenuPage() {
+    this.navController.push(MenuPage);
   }
 
 }
