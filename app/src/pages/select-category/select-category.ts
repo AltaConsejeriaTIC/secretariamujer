af049510c10e1788a3b03c06b5454be116971eda
Víctor Rodriguez <vrodmz@gmail.com>
2017-01-19 18:22:14 -0500

    Enum for violence categories added

--- app/src/pages/select-category/select-category.ts
+++ app/src/pages/select-category/select-category.ts
@@ -1,22 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, Injectable} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {SelectCategoryService} from "../../providers/select-category-service";
 import {TestPage} from "../test-page/test-page";
 import {MenuPage} from "../menu/menu";
+
+
+enum VIOLENCE_CATEGORIES {
+  ECONOMICAL = 0,
+  PHYSICAL = 1,
+  PSYCOLOGICAL = 2,
+  SEXUAL = 3
+}
 
 @Component({
   selector: 'page-select-category',
   templateUrl: './select-category.html'
 })
 export class SelectCategoryPage {
+  TEST_CATEGORIES = VIOLENCE_CATEGORIES;
+
   constructor(public navController: NavController, public selectCategoryService: SelectCategoryService) {
   }
 
   ionViewDidLoad() {
   }
 
-  selectCategory(categoryId: number) {
+  beginTest(categoryId: VIOLENCE_CATEGORIES) {
     this.selectCategoryService.setSelectedCategoryId(categoryId);
+    this.showTestPage();
+  }
+
+  private showTestPage() {
     this.navController.push(TestPage);
   }
 
