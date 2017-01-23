0fb7190755d6ed5b7108500ac6f06c22ffb2183c
AndresDiaz1 <andiazvar@gmail.com>
2017-01-23 10:19:06 -0500

    attention route locations now gets the background color according to the category

--- app/src/pages/attention-routes-location/attention-routes-location.ts
+++ app/src/pages/attention-routes-location/attention-routes-location.ts
@@ -8,14 +8,15 @@ import {NavController, NavParams} from 'ionic-angular';
 export class AttentionRoutesLocationPage {
 
   attentionRoute:AttentionRoute;
+  backgroundColor:string[]=['background-option-0','background-option-1','background-option-2','background-option-3'];
 
   constructor(public navCtrl: NavController, public navparams:NavParams) {
-
+    this.attentionRoute=this.navparams.get('attentionRoute');
+    console.log("lo que le llega", this.attentionRoute);
   }
 
   ionViewDidLoad() {
-    this.attentionRoute=this.navparams.get('attentionRoute');
-    console.log("lo que le llega", this.attentionRoute);
+
   }
 
   goToMenuPage() {
