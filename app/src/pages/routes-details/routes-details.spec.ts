b27b27eab3025daf833050b20b7f1bd94502aed6
AndresDiaz1 <andiazvar@gmail.com>
2017-01-23 15:40:38 -0500

    added unit test route details file missing from previous commit

---
+++ app/src/pages/routes-details/routes-details.spec.ts
@@ -0,0 +1,45 @@
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
+import {
+  App, MenuController, NavController, Platform, Config, Keyboard, Form, IonicModule,
+  NavParams
+}  from 'ionic-angular';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ConfigMock} from '../../mocks';
+import {RoutesDetailsPage} from "./routes-details";
+
+
+describe('RoutesDetailsPage tests', () => {
+
+  let routesDetailsPage: RoutesDetailsPage;
+  let fixture: ComponentFixture<RoutesDetailsPage>;
+  let stubNavParams={
+    get:()=>{
+      return{
+        id:0
+      }
+    }
+  };
+
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [RoutesDetailsPage],
+      providers: [
+        App, Platform, Form, Keyboard, MenuController, NavController,
+        {provide: Config, useClass: ConfigMock}, {provide: NavParams, useValue: stubNavParams}
+      ],
+      imports: [
+        FormsModule,
+        IonicModule,
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoutesDetailsPage);
+    routesDetailsPage = fixture.componentInstance;
+  });
+
+});
+
