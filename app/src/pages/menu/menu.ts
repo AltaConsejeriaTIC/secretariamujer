707d6c14d023e13c677c4a27ca67ba8b4a9caa49
Yesid Daza <yiye_91@hotmail.com>
2017-01-24 13:53:59 -0500

    Changes in style of main menu

--- app/src/pages/menu/menu.ts
+++ app/src/pages/menu/menu.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, trigger,state, style, transition, animate } from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {AlertCreator} from "../../providers/alert-creator";
 import {WelcomeTestPage} from "../welcome-test/welcome-test";
@@ -6,19 +6,36 @@ import {CallNumber} from 'ionic-native';
 
 @Component({
   selector: 'page-menu',
-  templateUrl: './menu.html'
+  templateUrl: './menu.html',
+  animations: [
+    trigger('menuItemHint', [
+      state('hintVisible', style({width:"50vw"})),
+      state('hiddenHint', style({width:"0vw"} )),
+      transition('hintVisible => hiddenHint', animate('100ms ease-in')),
+      transition('hiddenHint => hintVisible', animate('100ms ease-out'))
+    ]),
+    trigger('iconGroup', [
+      state('hintVisible', style({width:"40vw", padding: "0 0 0 0"})),
+      state('hiddenHint', style({width: "85vw", padding: "0 0 0 18vw"} )),
+      transition('hintVisible => hiddenHint', animate('100ms ease-out')),
+      transition('hiddenHint => hintVisible', animate('100ms ease-in'))
+    ]),
+  ]
 })
 export class MenuPage {
 
   menuOptions: any[];
+  items: any[] = [];
+  arrowIcon:string[]= [];
+  hintState:string[]= [];
 
   constructor(public navController: NavController, public alertCreator: AlertCreator) {
     this.menuOptions = [
       {
         isShowingHint: false,
         label: 'Información',
-        icon: 'home',
-        hint: '¿Qué debo saber si me encuentro en una situación violenta?',
+        icon: 'icon-info',
+        hint:  ['¿Qué debo saber si', 'me encuentro en una', 'situación violenta?'],
         goPage: function () {
           alertCreator.showSimpleAlert("Info", "Ir a la página de info está en desarrollo");
         }
@@ -26,8 +43,8 @@ export class MenuPage {
       {
         isShowingHint: false,
         label: 'Mapas',
-        icon: 'compass',
-        hint: '¿A dónde puedo ir si estoy en una situación violenta?',
+        icon: 'icon-map',
+        hint:  ['¿A dónde puedo ir', 'si estoy en una', 'situación violenta?'],
         goPage: function () {
           alertCreator.showSimpleAlert("Info", "Ir a la página de Mapas y rutas esta en desarrollo");
         }
@@ -35,8 +52,8 @@ export class MenuPage {
       {
         isShowingHint: false,
         label: 'Test / Tips y rutas',
-        icon: 'checkmark-circle',
-        hint: '¿Cómo puedo saber si estoy en una situación violenta?',
+        icon: 'icon-test',
+        hint:  ['¿Cómo puedo saber', 'si estoy en una', 'situación violenta?'],
         goPage: function () {
           navController.push(WelcomeTestPage);
         }
@@ -44,8 +61,8 @@ export class MenuPage {
       {
         isShowingHint: false,
         label: 'Configuración',
-        icon: 'cog',
-        hint: '¿A quién puedo dar aviso si me encuentro en riesgo?',
+        icon: 'icon-conf',
+        hint:  ['¿A quién puedo dar', 'aviso si me encuentro', 'en riesgo?'],
         goPage: function () {
           alertCreator.showSimpleAlert("Info", "Ir a la página de Configuracion esta en desarrollo");
         }
@@ -54,16 +71,31 @@ export class MenuPage {
   }
 
   ionViewDidLoad() {
+    this.setInitialIconAndHintState();
+  }
+
+  setInitialIconAndHintState() {
+    for (let i = 0; i < this.menuOptions.length; i++) {
+      this.arrowIcon.push("icon-border-next-arrow");
+      this.hintState.push("hiddenHint");
+    }
   }
 
   toggleisShowingHintParameter(index) {
     for (let i = 0; i < this.menuOptions.length; i++) {
       if (index == i) {
         this.menuOptions[index].isShowingHint = !this.menuOptions[index].isShowingHint;
+        this.setIconAndHintState(index, this.menuOptions[index].isShowingHint);
       } else {
         this.menuOptions[i].isShowingHint = false;
+        this.setIconAndHintState(i, this.menuOptions[i].isShowingHint);
       }
     }
+  }
+
+  setIconAndHintState(itemNumber, isShowingHint){
+    this.hintState[itemNumber] = (isShowingHint)? "hintVisible" : "hiddenHint";
+    this.arrowIcon[itemNumber] = (isShowingHint)? "icon-fill-back-arrow" : "icon-border-next-arrow";
   }
 
   makePhoneCall() {
