68cdec4b71fabb0b9b7a5f660ad6cc873359fca0
Yesid Daza <yiye_91@hotmail.com>
2017-01-31 13:25:30 -0500

    Options are visible depending on click in settings items

--- app/src/pages/settings-page/settings-page.ts
+++ app/src/pages/settings-page/settings-page.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+const SETTINGS_SLIDE_ITEMS = 3;
+
 @Component({
   selector: 'page-settings-page',
   templateUrl: 'settings-page.html'
 })
 export class SettingsPage {
 
+  arrowIconArray:string[]= [];
+  isOptionVisible:boolean[]=[];
+
   constructor(public navController: NavController) {}
 
   ionViewDidLoad() {
+    this.setInitialIconState();
+  }
 
+  setInitialIconState() {
+    for (let i = 0; i < SETTINGS_SLIDE_ITEMS; i++) {
+      this.arrowIconArray.push("icon-border-down-arrow");
+      this.isOptionVisible.push(false);
+    }
+  }
+
+  toggleisShowingOptions(index) {
+    for (let i = 0; i < SETTINGS_SLIDE_ITEMS; i++) {
+      this.isOptionVisible[i] = (index == i)? !this.isOptionVisible[index] : false;
+      this.setOptionState(i, this.isOptionVisible[i]);
+    }
+  }
+
+  setOptionState(optionNumber, optionState) {
+    this.arrowIconArray[optionNumber] = (optionState)? "icon-border-up-arrow" : "icon-border-down-arrow";
   }
 
   goToMenuPage() {
