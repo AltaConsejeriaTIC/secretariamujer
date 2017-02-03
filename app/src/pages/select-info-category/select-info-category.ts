2c2118c54450114aff0ffb700360ca8357cd7a2c
AndresDiaz1 <andiazvar@gmail.com>
2017-02-03 11:06:03 -0500

    created info category entity

--- app/src/pages/select-info-category/select-info-category.ts
+++ app/src/pages/select-info-category/select-info-category.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {InfoCategory} from "../../entity/info-categories";
 
 @Component({
   selector: 'page-select-info-category',
@@ -7,10 +8,19 @@ import { NavController } from 'ionic-angular';
 })
 export class SelectInfoCategoryPage {
 
-  constructor(public navCtrl: NavController) {}
+  infoCategories:InfoCategory[];
+
+  constructor(public navCtrl: NavController) {
+    this.infoCategories = [
+      {id: 0, labels: ['Sobre la Secretaria', 'de la Mujer'], class: 'option-0', RESTAddress:''},
+      {id: 1, labels: ['Sobre SOFIA'], class: 'option-1', RESTAddress:''},
+      {id: 2, labels: ['Sobre la App'], class: 'option-2', RESTAddress:''},
+      {id: 3, labels: ['Tutorial'], class: 'option-3', RESTAddress:''}
+    ];
+  }
 
   ionViewDidLoad() {
-    console.log('Hello SelectInfoCategoryPage Page');
+
   }
 
 }
