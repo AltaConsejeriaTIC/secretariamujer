4695740fb4dabeb6f9fc00ae9f69c05ca693e5be
Yesid Daza <yiye_91@hotmail.com>
2017-02-06 17:12:35 -0500

    Tutorial page is created

--- app/src/pages/select-info-category/select-info-category.ts
+++ app/src/pages/select-info-category/select-info-category.ts
@@ -4,6 +4,7 @@ import {InfoCategory} from "../../entity/info-categories";
 import {SDMUInfoPage} from "../sdmu-info-page/sdmu-info-page";
 import {SOFIAInfoPage} from "../sofia-info/sofia-info";
 import {AboutAppPage} from "../about-app-page/about-app-page";
+import {TutorialPage} from "../tutorial-page/tutorial-page";
 
 @Component({
   selector: 'page-select-info-category',
@@ -18,7 +19,7 @@ export class SelectInfoCategoryPage {
       {id: 0, labels: ['Sobre la Secretaria', 'de la Mujer'], class: 'option-0',iconName:'about-women-secretary', nextPage:'SDMUInfoPage'},
       {id: 1, labels: ['Sobre SOFIA'], class: 'option-1',iconName:'about-SOFIA', nextPage:'SOFIAInfoPage'},
       {id: 2, labels: ['Sobre la App'], class: 'option-2',iconName:'about-app', nextPage:'AboutAppPage'},
-      {id: 3, labels: ['Tutorial'], class: 'option-3',iconName:'icon-tutorial', nextPage:''}
+      {id: 3, labels: ['Tutorial'], class: 'option-3',iconName:'icon-tutorial', nextPage:'TutorialPage'}
     ];
   }
 
@@ -38,6 +39,10 @@ export class SelectInfoCategoryPage {
 
       case 'AboutAppPage':
         this.navController.push(AboutAppPage);
+        break;
+
+      case 'TutorialPage':
+        this.navController.push(TutorialPage);
         break;
     }
   }
