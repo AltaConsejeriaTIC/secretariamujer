1b5b79b3f37c536b9f37625954a73edbec64671d
Yesid Daza <yiye_91@hotmail.com>
2017-02-07 12:29:06 -0500

    Put images in tutorial page

--- app/src/pages/tutorial-page/tutorial-page.ts
+++ app/src/pages/tutorial-page/tutorial-page.ts
@@ -9,6 +9,7 @@ export class TutorialPage {
 
   sliderOptions: any;
   tutorialItems: any[];
+  tutorialBackgrounds: string[] = [];
 
   constructor(public navCtrl: NavController) {
     this.sliderOptions = {
@@ -16,15 +17,26 @@ export class TutorialPage {
     };
 
     this.tutorialItems = [
-      {image: "tutorial_1.png", label: 'Descubre si eres víctima de violencia de género por medio de la sección de Test'},
-      {image: "tutorial_2.png", label: 'Encuentra lugares donde podrás recibir atención por parte de personal profesional y capacitado casos de violencia de género'},
-      {image: "tutorial_3.png", label: 'Conoce y reporta los lugares más seguros de Bogotá en la sección de Mapa'},
-      {image: "tutorial_4.png", label: 'Encuentra tips especialmente pensados en ti para afrontar situaciones de violencia de género'},
-      {image: "tutorial_5.png", label: 'Activa el modo seguro y entra a la App digitando tu PIN en la calculadora y dandole en el igual'},
+      {label: 'Descubre si eres víctima de violencia de género por medio de la sección de Test'},
+      {label: 'Encuentra lugares donde podrás recibir atención por parte de personal profesional y capacitado casos de violencia de género'},
+      {label: 'Conoce y reporta los lugares más seguros de Bogotá en la sección de Mapa'},
+      {label: 'Encuentra tips especialmente pensados en ti para afrontar situaciones de violencia de género'},
+      {label: 'Activa el modo seguro y entra a la App digitando tu PIN en la calculadora y dandole en el igual'},
     ];
   }
 
   ionViewDidLoad() {
+    this.setTutorialBackgroundImages();
+  }
+
+  setTutorialBackgroundImages() {
+    for (let i = 0; i < this.tutorialItems.length; i++) {
+      this.tutorialBackgrounds.push(this.getURLImage(i+1));
+    }
+  }
+
+  getURLImage(imageNumber):string {
+    return 'url(../assets/img/tutorial_images/tutorial_'+ imageNumber + '.png)';
   }
 
   goToMenuPage() {
