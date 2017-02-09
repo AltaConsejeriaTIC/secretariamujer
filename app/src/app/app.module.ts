7e91b0c1a45f18b73bebc9ce9ed89e69bcfdcfc2
Richar Melo <rikmelos@gmail.com>
2017-02-09 16:24:51 -0500

    Added user service provider

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -37,6 +37,7 @@ import {CalculatorPage} from "../pages/calculator/calculator";
 import {UserNameFormPage} from "../pages/user-name-form/user-name-form";
 import {LoginService} from "../providers/login-service";
 import {UserFactory} from "../providers/user-factory";
+import {UserService} from "../providers/user-service";
 
 @NgModule({
   declarations: [
@@ -98,7 +99,7 @@ import {UserFactory} from "../providers/user-factory";
     UserNameFormPage
   ],
   providers: [UserDAO, AlertCreator, MapServices, EventsServices, TestsService,
-    ContactAdapter, Storage, WarningMessageDAO, FormValidator, LoginService, UserFactory,
+    ContactAdapter, Storage, WarningMessageDAO, FormValidator, LoginService, UserFactory,UserService,
     {
       provide: ErrorHandler,
       useClass: IonicErrorHandler
