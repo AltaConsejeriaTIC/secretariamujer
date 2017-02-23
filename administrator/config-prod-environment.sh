1c481620222509c8fa4199edd2a805ed50e7d61d
Víctor Rodriguez <vrodmz@gmail.com>
2017-02-23 11:12:37 -0500

    Script for set up the production enviroment

---
+++ administrator/config-prod-environment.sh
@@ -0,0 +1,10 @@
+sudo rm -rf /var/www/html/*
+sudo cp -R $HOME/secretariamujer/administrator/drupal/* /var/www/html/
+#sudo mkdir /var/www/html/sites/default/files
+#sudo mkdir /var/www/html/sites/default/files/translations
+#sudo chmod -R 777 /var/www/html/sites/default/files
+#sudo cp /var/www/html/sites/default/default.settings.php  /var/www/html/sites/default/settings.php
+#sudo chmod 777 /var/www/html/sites/default/settings.php
+sudo chown -R apache:apache /var/www/html/*
+sudo mysql -uroot -pSdmujer2017## < $HOME/secretariamujer/administrator/db/db.sql
+#sudo myslq -uroot -pSdmujer2017## < a2enmod rewrite
