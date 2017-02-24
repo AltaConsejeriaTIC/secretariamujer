4743ae4363b8ae69a150c78ba539817cc498a9b1
Víctor Rodriguez <vrodmz@gmail.com>
2017-02-24 17:12:29 -0500

    CordovaPluginCallNumber added to install manually

--- app/install.sh
+++ app/install.sh
@@ -3,19 +3,20 @@ mkdir www
 
 #npm install -g cordova ionic
 #ionic state reset
+npm install -g plugman
 npm install
 ionic prepare
 cordova plugin remove mx.ferreyra.callnumber
 
-wget https://github.com/Rohfosho/CordovaCallNumberPlugin/archive/master.zip
-unzip master.zip
-mv CordovaCallNumberPlugin-master CordovaCallNumberPlugin
-rm master.zip
+#wget https://github.com/Rohfosho/CordovaCallNumberPlugin/archive/master.zip
+#unzip master.zip
+#mv CordovaCallNumberPlugin-master CordovaCallNumberPlugin
+#rm master.zip
 
 plugman install --platform android --project platforms/android/ --plugin CordovaCallNumberPlugin/
-#plugman install --platform ios --project platforms/ios/ --plugin CordovaCallNumberPlugin/
+plugman install --platform ios --project platforms/ios/ --plugin CordovaCallNumberPlugin/
 
-rm -rf CordovaCallNumberPlugin
+#rm -rf CordovaCallNumberPlugin
 
 ionic build android
 mv ./platforms/android/build/outputs/apk/android-debug.apk ./sofiapp.apk
