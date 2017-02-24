3f72384d4871990245caf18d7634dc68dde81544
Víctor Rodriguez <vrodmz@gmail.com>
2017-02-24 16:21:07 -0500

    script for set up the development env in ionic

--- app/install.sh
+++ app/install.sh
@@ -1,7 +1,9 @@
 rm -rf platforms node_modules www plugins coverage cordova
-npm install -g cordova ionic
+mkdir www
+
+#npm install -g cordova ionic
+#ionic state reset
 npm install
-ionic state reset
 ionic prepare
 cordova plugin remove mx.ferreyra.callnumber
 
@@ -10,7 +12,10 @@ unzip master.zip
 mv CordovaCallNumberPlugin-master CordovaCallNumberPlugin
 rm master.zip
 
-plugman install --platform android --project platforms/android/ --plugin $HOME/CordovaCallNumberPlugin/
-plugman install --platform ios --project platforms/ios/ --plugin $HOME/CordovaCallNumberPlugin/
+plugman install --platform android --project platforms/android/ --plugin CordovaCallNumberPlugin/
+#plugman install --platform ios --project platforms/ios/ --plugin CordovaCallNumberPlugin/
 
 rm -rf CordovaCallNumberPlugin
+
+ionic build android
+mv ./platforms/android/build/outputs/apk/android-debug.apk ./sofiapp.apk
