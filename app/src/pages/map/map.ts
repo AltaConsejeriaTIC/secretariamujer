fec097b7cbb7f6713ab9f643edb77f2fcc8b9e39
Yesid Daza <yiye_91@hotmail.com>
2017-03-06 15:22:22 -0500

    map view shows google map

--- app/src/pages/map/map.ts
+++ app/src/pages/map/map.ts
@@ -1,101 +1,42 @@
-import {Component} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
-import {NavController} from 'ionic-angular';
-import {UserDAO} from  '../../providers/user-dao'
-import {MapServices} from  '../../providers/map-services'
-import {EventsServices} from "../../providers/events-services";
-import {AlertCreator} from "../../providers/alert-creator";
-import {Diagnostic} from 'ionic-native';
-
+declare var google;
 
 @Component({
   selector: 'page-map',
   templateUrl: './map.html'
 })
 export class MapPage {
+
+  @ViewChild('map') mapElement: ElementRef;
   map: any;
-  markers: any[] = [];
-  mapElement: any;
 
-  constructor(public navCtrl: NavController, public adminApi: UserDAO, public mapService: MapServices, public eventsServices: EventsServices, public alertCreator: AlertCreator) {
-  }
-
-  ionViewDidLoad() {
-    this.mapElement = document.getElementById('map');
-    this.createMap();
-    this.addMapOnClickListener();
-    this.isGPSEnabled();
-
-    google.maps.event.addListenerOnce(this.map, 'idle', () => {
-      this.mapElement.classList.add('show-map');
-      google.maps.event.trigger(this.mapElement, 'resize');
-    });
+  constructor(public navCtrl: NavController) {
 
   }
 
-  createMap() {
-    this.map = this.mapService.buildMap(this.mapElement);
+  ionViewDidLoad(){
+    this.loadMap();
   }
 
-  addMapOnClickListener() {
-    this.map.addListener('click', (event) => {
-      this.drawEventMarker(event);
-    });
-  }
-
-  drawEventMarker(event: any) {
-    if (this.isEventPinOnMap()) {
-      this.mapService.clearMarker(this.markers);
-    }
-    this.mapService.drawEventMarker(this.map, event.latLng, this.markers);
-  }
-
-  isGPSEnabled() {
-    Diagnostic.isLocationEnabled().then((res) => {
-      this.checkUserPosition(res);
-    }).catch((err) => {
-      this.alertCreator.showSimpleAlert("error", err);
-    });
-  }
-
-  checkUserPosition(isEnabled: boolean) {
-    if (isEnabled) {
-      this.getUserPosition();
-    } else {
-      this.alertCreator.showSimpleAlert('Error', 'Por favor activa el GPS');
-    }
-  }
-
-
-  getUserPosition() {
-    this.mapService.getUserLocation().then((position) => {
-      this.drawUserPosition(position);
-    }, (err) => {
-      console.log('hubo un error en la posciione del ussuriao', err);
-    });
-
-  }
-
-  drawUserPosition(position: any) {
-    let userPosition = this.mapService.convertToLatLng(position);
-    this.mapService.drawUserPositionMarker(this.map, userPosition);
-  }
-
-  isEventPinOnMap() {
-    return this.markers.length > 0;
-  }
-
-  registerEvent() {
-    this.eventsServices.registerEvent().map(res => res.json()).subscribe(response => {
-      this.alertCreator.showSimpleAlert('Exito', 'Se ha registrado el evento');
-    }, err => {
-      //this.alertCreator.showSimpleAlert('Error','Ha habido un error por favor intentalo más tarde');
-    });
-  }
-
-
-  goBackPage(){
+  goBackPage() {
     this.navCtrl.pop()
   }
 
+  goToMenuPage() {
+    this.navCtrl.popToRoot();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(4.6373802,-74.076619);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 }
