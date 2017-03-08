c344168a0a77ffabe72be9d511d3e73d36f6a758
Víctor Rodriguez <vrodmz@gmail.com>
2017-03-08 10:34:39 -0500

    Network plugin added

--- app/prod-release.sh
+++
@@ -1,9 +0,0 @@
-ionic build android --prod --release 
-mv platforms/android/build/outputs/apk/android-release-unsigned.apk ./
-echo "appporta" | jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
-zipalign -v 4 android-release-unsigned.apk app.apk
-
-rm android-release-unsigned.apk
-
-adb -d uninstall com.vivelabbogota.appsofia
-adb -d install app.apk 
