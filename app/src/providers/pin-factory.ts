62ce9e677cc560007a0f6113fab0466014213e72
Yesid Daza <yiye_91@hotmail.com>
2017-03-14 10:20:32 -0500

    Put pin popup styles in map

--- app/src/providers/pin-factory.ts
+++ app/src/providers/pin-factory.ts
@@ -10,7 +10,7 @@ export class PinFactory {
     this.sofiaPlaces = [
       {placeName: 'Secretaría 1', coordinate: [4.6341285,-74.0893915]},
       {placeName: 'Secretaría 2', coordinate: [4.6305157,-74.079887]},
-      {placeName: 'Secretaría 3', coordinate: [4.6293917,-74.0900739]},
+      {placeName: 'Instituto Nacional de Medicina Legal y Ciencias Forenses - UBAM (Valoración de menores de edad y casos de violencia sexual)', coordinate: [4.6293917,-74.0900739]},
       {placeName: 'Secretaría 4', coordinate: [4.6330481,-74.0871267]},
     ];
   }
@@ -33,10 +33,63 @@ export class PinFactory {
         'url': 'assets/maps/pinImages/info_pin.png'
       }
     });
-
-    let iconPlus ="<ion-icon md='md-add-circle' name='add-circle' role='img' class='icon icon-md ion-md-add-circle' aria-label='add circle' ng-reflect-name='add-circle' ng-reflect-md='md-add-circle'></ion-icon>";
-    let content = "<h4>" + placeName + "</h4>" + iconPlus;
+    let content = this.getPinContent("health", placeName);
     this.addInfoWindow(marker, content, infoWindow, map);
+  }
+
+  getPinContent(category, placeName) {
+    let categoryInfo = this.getCategoryInfoStructure(category);
+    let moreInfoButton = this.getMoreInfoButton();
+    return "<div class='pin-infowindow'>"+ categoryInfo + "<ion-label class='place-name'>" + placeName + "</ion-label>" + moreInfoButton + "</div>";
+  }
+
+  getMoreInfoButton() {
+    let moreInfoLabel = "<ion-label>Ver más</ion-label>";
+    let plusIcon ="<ion-icon md='md-add-circle' name='add-circle' role='img' class='icon icon-md ion-md-add-circle' aria-label='add circle' ng-reflect-name='add-circle' ng-reflect-md='md-add-circle'></ion-icon>";
+    return "<button ion-button='' class='disable-hover button button-ios button-default button-default-ios'><span class='button-inner'>" + plusIcon + moreInfoLabel + "</span><div class='button-effect'></div></button>";
+  }
+
+  getCategoryInfoStructure(category) {
+    let categoryInfoArray = this.getCategoryInfo(category);
+    let categoryIcon = this.getCategoryIconStructure(categoryInfoArray[0]);
+    let categoryName = this.getCategoryNameStructure(categoryInfoArray[1]);
+    return "<div class='category-title'>" + categoryIcon + categoryName + "</div>";
+  }
+
+  getCategoryInfo(category) {
+    let categoryIcon = "";
+    let categoryName = "";
+    switch (category) {
+      case 'info': {
+        categoryIcon = "icon-info-places";
+        categoryName = "Puntos de Información";
+        break;
+      }
+      case 'justice': {
+        categoryIcon = "icon-complaint-places";
+        categoryName = "Puntos para el Acceso a la Justicia";
+        break;
+      }
+      case 'protection_measures': {
+        categoryIcon = "icon-protection-measures";
+        categoryName = "Medidas de Protección";
+        break;
+      }
+      case 'health': {
+        categoryIcon = "icon-health-places";
+        categoryName = "Puntos de Salud";
+        break;
+      }
+    }
+    return [categoryIcon, categoryName];
+  }
+
+  getCategoryIconStructure(categoryIcon) {
+    return "<ion-icon class='icon-col icon icon-md ion-md-" + categoryIcon + "' name='"+ categoryIcon + "'></ion-icon>";
+  }
+
+  getCategoryNameStructure(categoryName) {
+    return "<ion-label>" + categoryName + "</ion-label>";
   }
 
   addInfoWindow(marker, content, infoWindow, map){
