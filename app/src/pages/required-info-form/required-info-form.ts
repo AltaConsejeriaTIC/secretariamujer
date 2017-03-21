9a78fab3dbe1d48d9e16b9e5a4df9de9b26e2388
AndresDiaz1 <andiazvar@gmail.com>
2017-03-21 10:34:44 -0500

    app now saves data for offline use, at register, and login

--- app/src/pages/required-info-form/required-info-form.ts
+++ app/src/pages/required-info-form/required-info-form.ts
@@ -8,6 +8,10 @@ import {UserService} from "../../providers/user-service";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import {FormValidator} from "../../providers/form-validator";
 import {NetworkStatusService} from "../../providers/network-status-service";
+import {Platform} from 'ionic-angular';
+import { File } from 'ionic-native';
+import {OfflineService} from "../../providers/offline-service";
+declare var cordova:any;
 
 
 @Component({
@@ -21,9 +25,10 @@ export class RequiredInfoFormPage {
   form: FormGroup;
   username: string;
   password: string;
+  dataDirectory: string;
 
-
-  constructor(public navCtrl: NavController, public alertCreator: AlertCreator, public userDAO: UserDAO, private userService: UserService, public loadingController: LoadingController, private  formBuilder: FormBuilder, public formValidator: FormValidator,) {
+  constructor(public navCtrl: NavController, public alertCreator: AlertCreator, public userDAO: UserDAO, private userService: UserService, public loadingController: LoadingController, private  formBuilder: FormBuilder, public formValidator: FormValidator,public offlineService:OfflineService, public platform: Platform) {
+    this.dataDirectory=cordova.file.dataDirectory;
     this.user = new User();
     this.loading = this.createLoading();
     this.createForm(formBuilder);
@@ -82,8 +87,14 @@ export class RequiredInfoFormPage {
       .subscribe(userId => {
         this.userService.user.id = userId;
         this.alertCreator.showCofirmationMessage('Cuenta', 'Tu cuenta ha sido creada', () => {
-          this.hideLoading();
-          this.navCtrl.push(RegisterOptionalInfoPage);
+
+          if(this.platform.is('cordova')){
+            this.setOfflineContent();
+          }else{
+            this.hideLoading();
+            this.navCtrl.push(RegisterOptionalInfoPage);
+          }
+
         })
       }, error => {
         this.hideLoading();
@@ -97,6 +108,46 @@ export class RequiredInfoFormPage {
         }
 
       });
+  }
+
+  setOfflineContent(){
+    this.offlineService.getAllAppData().subscribe((data)=>{
+      console.log("toda la data", data);
+      this.writeFiles(data);
+    },(err)=>{
+      console.log("error get all data", err);
+      this.hideLoading();
+      this.navCtrl.push(RegisterOptionalInfoPage);
+    });
+
+  }
+
+  writeFiles(data){
+    Promise.all([
+      File.writeFile(this.dataDirectory,'categoriesTitles.txt',data[0],{replace:true}),
+      File.writeFile(this.dataDirectory,'testOneQuestions.txt',data[1],{replace:true}),
+      File.writeFile(this.dataDirectory,'testTwoQuestions.txt',data[2],{replace:true}),
+      File.writeFile(this.dataDirectory,'testThreeQuestions.txt',data[3],{replace:true}),
+      File.writeFile(this.dataDirectory,'testFourQuestions.txt',data[4],{replace:true}),
+      File.writeFile(this.dataDirectory,'tipsOne.txt',data[5],{replace:true}),
+      File.writeFile(this.dataDirectory,'tipsTwo.txt',data[6],{replace:true}),
+      File.writeFile(this.dataDirectory,'tipsThree.txt',data[7],{replace:true}),
+      File.writeFile(this.dataDirectory,'tipsFour.txt',data[8],{replace:true}),
+      File.writeFile(this.dataDirectory,'infoRoutes.txt',data[9],{replace:true}),
+      File.writeFile(this.dataDirectory,'healthRoutes.txt',data[10],{replace:true}),
+      File.writeFile(this.dataDirectory,'justiceRoutes.txt',data[11],{replace:true}),
+      File.writeFile(this.dataDirectory,'protectionRoutes.txt',data[12],{replace:true}),
+      File.writeFile(this.dataDirectory,'aboutSDMU.txt',data[13],{replace:true}),
+      File.writeFile(this.dataDirectory,'aboutSOFIA.txt',data[14],{replace:true}),
+      File.writeFile(this.dataDirectory,'aboutApp.txt',data[15],{replace:true}),
+    ]).then(()=>{
+      this.hideLoading();
+      this.navCtrl.push(RegisterOptionalInfoPage);
+    }).catch((err)=>{
+      console.log("error writeFiles", err);
+      this.hideLoading();
+      this.navCtrl.push(RegisterOptionalInfoPage);
+    });
   }
 
   userCanContinue() {
